b9996da173e23cac93b7d684f053ede9b0e80e6f
0zone-Layer <rahulvaizi690@gmail.com>
2026-03-17 15:03:03 +0530

    Refactor Redis initialization to use fromEnv method

--- api/load.js
+++ api/load.js
@@ -1,6 +1,5 @@
 import { Redis } from "@upstash/redis";
-
-const redis = new Redis({
+const redis = Redis.fromEnv();
   url:   process.env.UPSTASH_REDIS_REST_URL,
   token: process.env.UPSTASH_REDIS_REST_TOKEN,
 });
