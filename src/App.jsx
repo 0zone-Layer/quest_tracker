cb23d81ba481b66b6a02803cd61bc47c32186388
0zone-Layer <rahulvaizi690@gmail.com>
2026-03-17 15:01:51 +0530

    Update route paths for QuestTracker and PublicProfile

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import QuestTracker  from "./QuestTracker";
+import QuestTracker from "./QuestTracker";
 import PublicProfile from "./PublicProfile";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/"  element={<QuestTracker />} />
-        <Route path="/p" element={<PublicProfile />} />
+        <Route path="/" element={<QuestTracker />} />
+        <Route path="/profile" element={<PublicProfile />} />
       </Routes>
     </BrowserRouter>
   );
